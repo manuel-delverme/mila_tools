a72339a5e7c2ff5ee964b4721a2609b4d7e9d104
ioneliabuzatu <buzatuionelia@gmail.com>
2020-12-08 19:58:26 +0100

    strating to fix automatic git

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 
 install_requires = [
-    'GitPython', 'tensorboardX', 'matplotlib', 'wandb', 'fabric', 'cloudpickle', 'jax', 'tkinter'
+    'GitPython', 'tensorboardX', 'matplotlib', 'wandb', 'fabric', 'cloudpickle', 'jax'
 ]
 
 setup(
@@ -14,7 +14,8 @@ setup(
     ),
     url='github.com/ministry-of-silly-code/experiment_buddy/',
     license='',
-    author='Manuel Del Verme',
+    author='Manuel Del Verme, Ionelia Buzatu',
+    maintainer='Manuel Del Verme',
     author_email='',
     description='',
     install_requires=install_requires,
