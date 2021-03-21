7116d762e2169570756fb1f7091e926edf92b427
manuel <manuel.delverme@gmail.com>
2021-03-21 10:58:19 +0100

    Ignoring non parameter arguments, see issue #7

--- experiment_buddy/experiment_buddy.py
+++ experiment_buddy/experiment_buddy.py
@@ -45,7 +45,9 @@ def register(config_params):
     for arg in sys.argv[1:]:
         if arg.endswith(".py"):
             continue
-        assert arg[:2] == "--", arg
+        if not arg.startswith("--"):
+            continue
+
         k, v = arg[2:].split("=")
         k = k.lstrip(wandb_escape)
         v = _cast_param(v)
