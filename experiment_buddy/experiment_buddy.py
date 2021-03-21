f89f7a58be19eb158b0ed5a88fec70d5e574e49a
ioneliabuzatu <buzatuionelia@gmail.com>
2021-03-21 16:34:36 +0100

    temporarily disable window

--- experiment_buddy/experiment_buddy.py
+++ experiment_buddy/experiment_buddy.py
@@ -188,7 +188,7 @@ def deploy(host: str = "", sweep_yaml: str = "", proc_num: int = 1, entity=None,
         tb_dir = os.path.join(git_repo.working_dir, ARTIFACTS_PATH, "tensorboard/", experiment_id, dtm)
         return WandbWrapper(f"{experiment_id}_{dtm}", local_tensorboard=_setup_tb(logdir=tb_dir), **wandb_kwargs)
 
-    experiment_id = _ask_experiment_id(host, sweep_yaml)
+    experiment_id = "debug" # _ask_experiment_id(host, sweep_yaml)
     print(f"experiment_id: {experiment_id}")
     if local_run:
         tb_dir = os.path.join(git_repo.working_dir, ARTIFACTS_PATH, "tensorboard/", experiment_id, dtm)
