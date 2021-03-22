37a10e102b4934e005af87921c1a3eea7d7a6935
ioneliabuzatu <buzatuionelia@gmail.com>
2021-03-22 14:14:43 +0100

    move return

--- experiment_buddy/utils.py
+++ experiment_buddy/utils.py
@@ -1,10 +1,10 @@
-import time
 import invoke
 
 
 def check_if_has_slurm(ssh_session):
     try:
         has_slurm = ssh_session.run("/opt/slurm/bin/scontrol ping")
-        return has_slurm.ok
     except invoke.exceptions.UnexpectedExit:
         return False
+
+    return has_slurm.ok
