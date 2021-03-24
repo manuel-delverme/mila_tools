9a73df303ce3fa7a5f26789960ea2998670f2f31
ioneliabuzatu <buzatuionelia@gmail.com>
2021-03-24 16:10:42 +0100

    parse project name function

--- experiment_buddy/utils.py
+++ experiment_buddy/utils.py
@@ -1,6 +1,8 @@
 import invoke
 import os
 from git.util import IterableList
+import git
+
 
 def check_if_has_slurm(ssh_session):
     try:
@@ -10,10 +12,10 @@ def check_if_has_slurm(ssh_session):
 
     return has_slurm.ok
 
-def project_name(git_repo):
-    git_repo_remotes = git_repo.remotes
-    if isinstance(git_repo_remotes, IterableList):
-        remote_url = git_repo_remotes.config_reader.get("url")
-        git_project_name = os.path.splitext(os.path.basename(remote_url))[0]
-        return git_project_name
 
+def get_project_name(git_repo):
+    git_repo_remotes = git_repo.remotes
+    assert isinstance(git_repo_remotes, IterableList)
+    remote_url = git_repo_remotes[0].config_reader.get("url")
+    project_name, _ = os.path.splitext(os.path.basename(remote_url))[0]
+    return project_name
