394c47e4de529f07e01743e796ee34eb4bcdf04e
manuel <manuel.delverme@gmail.com>
2021-10-29 18:39:25 +0200

    Removed telemetry leftovers

--- experiment_buddy/utils.py
+++ experiment_buddy/utils.py
@@ -1,14 +1,10 @@
-import asyncio
-import atexit
 import enum
 import os
 import subprocess
 
-import aiohttp
 import fabric
 import git
 import invoke
-from funcy import log_durations
 
 
 class Backend(enum.Enum):
@@ -45,36 +41,7 @@ def get_project_name(git_repo: git.Repo) -> str:
     return project_name
 
 
-def fire_and_forget(f):
-    def wrapped(*args, **kwargs):
-        return asyncio.ensure_future(f(*args, *kwargs))
-
-    return wrapped
-
-
-def __async_cleanup():
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(asyncio.gather(*asyncio.all_tasks(loop)))
-
-
-atexit.register(__async_cleanup)
-
-
-@fire_and_forget
-async def __remote_time_logger(elapsed: str):
-    import re
-    elapsed, function_name = re.search(r'([\d.]+).+in (\w+)', elapsed).groups()
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(
-                f'http://static.92.155.21.65.clients.your-server.de/{function_name}/{elapsed}') as response:
-            await response.text()
-
-
 def _get_job_info(jid):
     result = subprocess.check_output(f"/opt/slurm/bin/sacct --brief -j {jid}".split()).decode()
     jid, state, exit_code = result.split("\n")[2].split()
     return state
-
-
-telemetry = log_durations(__remote_time_logger, unit='s')
