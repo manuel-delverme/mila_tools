be448ec2144e6e8763cc3fabc7573a9789b7f206
manuel <manuel.delverme@gmail.com>
2021-11-17 18:07:19 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
 
 setup(
     name='experiment_buddy',
-    version='0.0.4',
+    version='0.0.5',
     packages=["experiment_buddy", "scripts"],
     package_data={'scripts': ['*/*.sh']},
     url='https://github.com/ministry-of-silly-code/experiment_buddy/',
